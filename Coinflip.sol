64a7477fa383457de710465bf9ea51bc699a65e1
Luis Egea <jshanks1001@gmail.com>
2020-02-15 07:58:16 -0800

    initial commit

---
+++ Coinflip.sol
@@ -0,0 +1,36 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+contract Coinflip {
+  address public owner;
+  mapping(address => uint) public balances;
+  mapping(address => bool) public lastFlip;
+
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  function getBalance(address _addr) public view returns (uint){
+    return balances[_addr];
+  }
+
+  function getLastFlip(address _player) public view returns (bool) {
+      return lastFlip[_player];
+  }
+
+  function flip() public payable {
+    uint time = block.timestamp;
+    uint bet = msg.value;
+
+    if(time % 2 == 0){      //Requires a more effective random number algorithm for production use.
+      msg.sender.transfer(bet*2);
+      lastFlip[msg.sender] = true;
+    }
+    else{
+        lastFlip[msg.sender] = false;
+    }
+  }
+
+  function deposit() public payable {
+
+  }
+}
